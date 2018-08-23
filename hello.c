80194a3a23fc8c319b87cfe2e6a454093c6f9c46
Dspotanski <42529598+Dspotanski@users.noreply.github.com>
2018-08-23 12:51:29 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dylan Spotanski and Renee Murphy
+ * Date: 2018/21/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dylan Spotanski and Renee Murphy\n");
+
+  return 0;
+}
